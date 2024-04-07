cdafbb84a2fcea8442b6709fab1d047b98d39d7c
andavtyan <anna.davtyan7@tumo.org>
2024-04-07 19:43:09 +0400

    server

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+const express = require('express')
+const path = require('path')
+const http = require("http");
+const socket = require("socket.io");
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")));
+const server = http.createServer(app)
+
+const io = socket(server);
+
+io.on('connection', (socket)=>{
+   console.log('user connected');
+   socket.on('disconnect', ()=>{
+    io.emit("message", "A user disconnected!")
+ })
+})
+
+server.listen(3000, () => {
+  console.log("Server listens to port 3000");
+});
